26b5c0819d34cc196ec3d5afbc61cbdf9c873ba1
correaa1 <bruno_correa_ds@hotmail.com>
2021-03-11 23:14:26 -0300

    Start Project

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { Audio } from "expo-av";
+import { Container } from "./styles";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [sound, setSound] = React.useState();
+    async function playSound() {
+        console.log("Loading Sound");
+        const { sound } = await Audio.Sound.createAsync(require("./assets/CavaloAudio.mp3"));
+        setSound(sound);
+
+        console.log("Playing Sound");
+        await sound.playAsync();
+    }
+
+    React.useEffect(() => {
+        return sound
+            ? () => {
+                  console.log("Unloading Sound");
+                  sound.unloadAsync();
+              }
+            : undefined;
+    }, [sound]);
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity title="Play Sound" onPress={playSound}>
+                <Image source={require("./Imagens/Cavalo.jpg")} style={styles.imagens} onPress={() => this.moveToAddNewCustomer()} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Image source={require("./Imagens/Cachorro.jpg")} style={styles.imagens} onPress={() => this.moveToAddNewCustomer()} />
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    imagens: {
+        resizeMode: "center",
+        width: 250,
+        height: 200,
+    },
 });
---
+++ styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    display: flex;
+    background-color: #fff;
+`;
+export const TouchableOpacity = styled.View`
+    width: 250px;
+    height: 200px;
+`;
