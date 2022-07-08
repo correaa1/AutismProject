468ad3ff23522ec4b8b28286f97df125576af69e
correaa1 <bruno_correa_ds@hotmail.com>
2022-07-08 12:00:32 -0300

    segunda tela recuperada

--- pages/Scenes/styles.js
+++ pages/Scenes/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components/native";
 
-export const Container = styled.ScrollView`
+export const Container = styled.View`
     display: flex;
     background-color: #fff;
     height: 100%;
@@ -8,24 +8,36 @@ export const Container = styled.ScrollView`
 `;
 export const Cloud = styled.View`
     position: absolute;
-    right: 10%;
+    left: 5%;
+    top: 10%;
+    width: 30%;
 `;
 export const Cloud2 = styled.View`
     position: absolute;
+    top: 10%;
+    right:1%;
+    width: 30%;
+
 `;
 export const House = styled.View`
     position: absolute;
-    width: 50%;
+    width: 30%;
+    height:60%;
+    top:100%
+    ;
 `;
 export const Sun = styled.View`
  position: absolute;
-    top: 2%;
-    right:5%;
+    top: 10%;
+    right:33%;
     width: 30%;
 
 `;
 export const Tree = styled.View`
  position: absolute;
-    width: 30%;
+    width: 20%;
+    height:60% ;
+    left: 30%;
+    top:100%;
 
 `;
