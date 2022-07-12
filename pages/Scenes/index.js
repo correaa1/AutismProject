a58376ce9a600b019ca87e1060c28fcc4c8c0d14
correaa1 <bruno_correa_ds@hotmail.com>
2022-07-12 10:41:39 -0300

    inserindo audio nas imagens da tela de scenes

--- pages/Scenes/index.js
+++ pages/Scenes/index.js
@@ -3,12 +3,41 @@ import { Text, Image, Button, View, TouchableOpacity } from "react-native";
 
 import ButtonImage from "./components/ButtonImage";
 
-import { Container,  Cloud, Cloud2, Home,Sun,Tree,Tree2,Gram,Bird,Plane } from "./styles";
+import { Container,  Cloud, Cloud2, Home,Sun,Tree,Tree2,Gram,Bird,Plane,ImageAnimals, ImageBackgroundAnimals, ButtonAction, ViewAnimalsAction, TextAnimalName, ImageBackground } from "./styles";
 function Scenes2() {
+  
+    const [sound, setSound] = React.useState();
+
+    /*Abaixo é o array dos animais, cada elemento do array tem outro array dentro.
+     O array de dentro é composto pelo nome do animal, require da imagem, e o require do som.
+     Ou seja, para mudar o animal basta mudar o nome do diretorio e caso necessário também o formato.*/
+
+    const animals = [
+        require(`./sound/Wolf.mp3`)
+    ]
+    async function playSound(animalsound) {
+        console.log("Loading Sound");
+        const { sound } = await Audio.Sound.createAsync(animalsound);
+        ("  ");
+        setSound(sound);
+
+        console.log("Playing Sound");
+        await sound.playAsync();
+    }
+    React.useEffect(() => {
+        return sound
+            ? () => {
+                  console.log("Unloading Sound");
+                  sound.unloadAsync();
+              }
+            : undefined;
+    }, [sound]);
+
+
     return (
         <Container>
             <Cloud>
-                <ButtonImage imagePath={require("./src/Cloud.png")} initialOpacity={0.4} />
+                <ButtonImage onPress={() => {playSound(animals[0])}} imagePath={require("./src/Cloud.png")} initialOpacity={0.4} />
             </Cloud>
             <Cloud2>
                 <ButtonImage  imagePath={require("./src/cloud2.png")} initialOpacity={0.3} />
@@ -45,5 +74,6 @@ function Scenes2() {
         </Container>
     );
 }
+
 
 export default Scenes2;
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,14 +1,14 @@
 import { View, Text, Image, TouchableOpacity, BackHandler } from "react-native";
 import  React, {useState} from 'react';
 import { Button } from 'react-native-paper';
-import { Container,Card,Texto} from "./styles";
+import { Container,Card,Texto, } from "./styles";
 import Animals from "../animals/index"
 import Home from "../home/index"
 import Scenes from "../Scenes/index"
   
 export default function App(){
 
-
+ 
 const [page, setPage] = useState(0);
   const currentPage = [<></>, <Animals />, <Scenes /> ];
   return(<>
