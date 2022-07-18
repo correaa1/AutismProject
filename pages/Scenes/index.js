af43299d584137ba1e11b2a7abc038cd88000d27
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2022-07-18 18:54:18 -0300

    fix_sound_button

--- pages/Scenes/index.js
+++ pages/Scenes/index.js
@@ -1,31 +1,20 @@
 import React, { useState } from "react";
 import { Text, Image, Button, View, TouchableOpacity } from "react-native";
 import { Audio } from "expo-av";
+import ButtonColor from './components/ButtonImage/index'
 
 
 import { Container,  Cloud, Cloud2, Home,Sun,Tree,Tree2,Gram,Bird,Plane} from "./styles";
 function Scenes2() {
-  
-  
 
     const [sound, setSound] = React.useState();
 
-    /*Abaixo é o array dos animais, cada elemento do array tem outro array dentro.
-     O array de dentro é composto pelo nome do animal, require da imagem, e o require do som.
-     Ou seja, para mudar o animal basta mudar o nome do diretorio e caso necessário também o formato.*/
-
-    const animals = [
-        require(`./sound/Wolf.mp3`)
-    ]
     async function playSound(animalsound) {
-        console.log("Loading Sound");
         const { sound } = await Audio.Sound.createAsync(animalsound);
-        ("  ");
         setSound(sound);
-
-        console.log("Playing Sound");
         await sound.playAsync();
     }
+
     React.useEffect(() => {
         return sound
             ? () => {
@@ -34,24 +23,36 @@ function Scenes2() {
               }
             : undefined;
     }, [sound]);
-    const [opacity, setOpacity] = useState(0.1);
 
-    function ButtonImage({ imagePath, initialOpacity = 0.1, sound }) {
-      
-      
-        
-       
-        return (
-            <TouchableOpacity activeOpacity={0.1} onPress={()  => {playSound(animals[sound]); setOpacity(5); }}>
-                <Image source={imagePath} opacity={opacity} />
-            </TouchableOpacity>
-        );
-    }
+    const items = {
+        "exemplo" : {
+            image: require("./src/cloud2.png"), //aqui voce coloca o path da imagem
+            sound: require(`./sound/Wolf.mp3`), //aqui o path do som
+        },
+        "nuvem" : {
+            image: require("./src/cloud2.png"),
+            sound: require(`./sound/Wolf.mp3`),
+        },
+    }; // para usar você so precisa colocar... items.exemplo.image ai ele pega a imagem...
+
 
     return (
         <Container>
-            <Cloud>
-                <ButtonImage sound={0} imagePath={require("./src/Cloud.png")} initialOpacity={0.4} />
+
+            {/* Abaxio segue um exemplo usando o objeto de items */}
+            <ButtonColor imagePath={items.nuvem.image} playSoundFunction={() => playSound(items.nuvem.sound)} /> 
+            
+            <Home>
+            <ButtonColor imagePath={items.nuvem.image} playSoundFunction={() => playSound(items.nuvem.sound)} /> 
+            </Home>
+
+            <Sun>
+            <ButtonColor imagePath={items.nuvem.image} playSoundFunction={() => playSound(items.nuvem.sound)} /> 
+           </Sun> 
+            
+           
+            {/* <Cloud>
+                <ButtonImage sound={0} imagePath={require("./src/Cloud.png")} />
             </Cloud>
             <Cloud2>
                 <ButtonImage onPress={() => {playSound(animals[0])}}  imagePath={require("./src/cloud2.png")} initialOpacity={0.3} />
@@ -61,16 +62,16 @@ function Scenes2() {
             </Home>
            <Sun>
                 <ButtonImage onPress={() => {playSound(animals[0])}} imagePath={require("./src/sun.png")} initialOpacity={0.2} />
-           </Sun>
+           </Sun> */}
           
-           <Tree> 
+           {/* <Tree> 
                <ButtonImage onPress={() => {playSound(animals[0])}} imagePath={require("./src/Tree1.png")} initialOpacity={0.2} />
            </Tree>
          
            <Tree2> 
                <ButtonImage onPress={() => {playSound(animals[0])}} imagePath={require("./src/Tree1.png")} initialOpacity={0.2} />
-           </Tree2>
-
+           </Tree2> */}
+{/* 
            <Gram>
          
            </Gram>
@@ -83,7 +84,7 @@ function Scenes2() {
 
         <Plane>
         <ButtonImage  imagePath={require("./src/Plane.png")} initialOpacity={0.3} />
-        </Plane>
+        </Plane> */}
 
         </Container>
     );
